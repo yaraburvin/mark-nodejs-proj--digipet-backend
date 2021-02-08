b82505b43b962b438075af621078dd6bafc1c9bd
Richard Ng <richardcrng@gmail.com>
2021-02-08 15:49:01 +0000

    Write tests on resource creation

--- src/server.test.ts
+++ src/server.test.ts
@@ -11,13 +11,46 @@ describe("Server initialisation", () => {
 });
 
 describe("Endpoint testing", () => {
-  test("GET /signatures retrieves the first signature", async () => {
+  test("GET /signatures retrieves the initial signature", async () => {
     const response = await supertest(app).get("/signatures");
+
+    // check that it's a 200 response
+    expect(response.status).toBe(200);
+
+    // check the overall structure of the response
     expect(response.body).toMatchObject({
       status: "success",
       data: {
         signatures: [{ name: "Rick" }],
       },
     });
+
+    // check that it comes back with only one signature
+    expect(response.body.data.signatures).toHaveLength(1);
+
+    // check that the signature has a string timestamp
+    // (we don't know what it will be at test writing)
+    expect(typeof response.body.data.signatures[0]).toBe("string");
+  });
+
+  test("POST /signatures adds a signature", async () => {
+    const response = await supertest(app).post("/signatures").send({
+      name: "Morty",
+      message: "ah, geez",
+    });
+
+    // check that it's a 201 response
+    expect(response.status).toBe(201);
+
+    // check the overall structure of the response includes the created resource
+    expect(response.body).toMatchObject({
+      status: "success",
+      data: {
+        signature: { name: "Morty", message: "ah, geez" },
+      },
+    });
+
+    // check that it has added a timesstamp
+    expect(typeof response.body.data.signature.timestamp).toBe("string");
   });
 });
