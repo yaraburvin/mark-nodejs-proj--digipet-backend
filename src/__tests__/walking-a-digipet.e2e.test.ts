74a0b0e47a7fee59a52f0fd59a3d4fd499ca1c07
Richard Ng <richardcrng@gmail.com>
2021-02-09 08:05:23 +0000

    Add explanatory comment on separation of tests

--- src/__tests__/walking-a-digipet.e2e.test.ts
+++ src/__tests__/walking-a-digipet.e2e.test.ts
@@ -2,6 +2,16 @@ import supertest from "supertest";
 import { Digipet, setDigipet } from "../digipet/model";
 import app from "../server";
 
+/**
+ * This file has end-to-end (E2E) tests for walking a digipet.
+ *
+ * It is intended to test two behaviours:
+ *  1. walking a digipet leads to increasing happiness
+ *  2. walking a digipet leads to decreasing nutrition
+ *
+ * These have been mostly separated out into two different E2E tests to try to make the tests more robust - it is possible that we might want a change in one but not the other, and it would be annoying to have to fix tests on increasing happiness when there's a change in intended nutrition behaviour.
+ */
+
 describe("When a user walks a digipet repeatedly, its happiness increases by 10 each time until it eventually maxes out at 100", () => {
   beforeAll(() => {
     // setup: give an initial digipet
